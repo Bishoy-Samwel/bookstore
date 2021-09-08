33816cff72826767f33b5f2f6fc6628f4dc952e4
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-08 16:21:15 +0200

    comment provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    {/* <Provider> */}
+    <Router>
+      <App />
+    </Router>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
