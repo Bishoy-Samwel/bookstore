56d1eed7270ca62bee9f563fa85add0bfc4fa266
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-08 10:33:14 +0200

    Use Switch and Route for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Container  from './components/Container';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Container/>
     </div>
   );
 }
