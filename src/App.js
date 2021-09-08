e38d20fdb79579be0fed19664d4aae9c06bc61a0
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-08 13:47:40 +0200

    linters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Container  from './components/Container';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-      <Container/>
+      <Container />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
-import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -23,4 +22,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
