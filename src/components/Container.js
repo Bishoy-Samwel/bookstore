714e16ae4f7bf9b986b387bf460f16a88b1e4be4
Bishoy <bishoy.samwuel@gmail.com>
2021-09-10 15:46:30 +0200

    Add main methods to container

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,18 +4,38 @@ import {
   Route,
 } from 'react-router-dom';
 
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import Navbar from './NavBar';
 import Categories from './Categories';
 import Books from './Books';
 import AddBookForm from './AddBookForm';
+import { addBook, removeBook } from '../redux/books/books';
 
 export default function Container() {
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (book) => {
+    const newBook = {
+      id: uuidv4(), // make sure it's unique
+      title: book.title,
+      author: book.author,
+    };
+    dispatch(addBook(newBook));
+  };
+
+  const removeBookFromStore = (id) => {
+    dispatch(removeBook(id));
+  };
+
+  const books = useSelector((state) => state.books, shallowEqual);
+
   return (
     <>
       <Navbar />
       <Switch>
         <Route exact path="/">
-          <Books />
+          <Books books={books} remove={removeBookFromStore} />
         </Route>
         <Route path="/books">
           <Books />
@@ -24,7 +44,7 @@ export default function Container() {
           <Categories />
         </Route>
       </Switch>
-      <AddBookForm />
+      <AddBookForm add={submitBookToStore} />
     </>
   );
 }
