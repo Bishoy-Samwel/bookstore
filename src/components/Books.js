cf9bc026e5895ddb2f63e50267909163fdbfa695
Bishoy <bishoy.samwuel@gmail.com>
2021-09-10 15:45:46 +0200

    Remove code to another components

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,34 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import React from 'react';
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../redux/books/books';
-
 import Book from './Book';
 
-const dispatch = useDispatch();
-
-const submitBookToStore = (title, author) => {
-  const newBook = {
-    id: uuidv4(), // make sure it's unique
-    title,
-    author,
-  };
-  dispatch(addBook(newBook));
-};
-
-const removeBookFromStore = (id) => {
-  dispatch(removeBook(id));
-};
-
-function books() {
-  const books = useSelector((state) => state.counter, shallowEqual);
-
+export default function Books(props) {
+  const { books, remove } = props;
   return (
     <ul>
-      {books.map((book) => <Book book={book} />)}
+      {books.map((book) => <Book book={book} remove={remove} />)}
     </ul>
   );
 }
-
-export { books, submitBookToStore, removeBookFromStore };
